82f53f7cc84eecae3377a5797f08956754b063ff
Rounak Singh <rounaksingh@Rounaks-MacBook-Air.local>
2020-12-04 21:40:42 +0530

    first commit

---
+++ api/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    dbHost : 'localhost',
+    dbName : 'notes-app',
+    dbCollection : 'notes-data'
+};
---
+++ api/routes/index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+
+router.get('/', function(req, res, next) {
+
+  const { uid } = req.body;
+  req.collection.find({uid:uid})
+  .toArray()
+  .then(results => res.json(results))
+  .catch(error => res.send(error))
+});
+
+router.post('/', function(req, res, next){
+  const { uid , note } = req.body;
+  const payload = { uid, note};
+  req.collection.insertOne(payload)
+  .then(result=>res.json(result))
+  .catch(error=>res.send(error));
+});
+
+module.exports = router;
